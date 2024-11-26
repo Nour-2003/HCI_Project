4bfa5ed47d61e01e121ffb9ec03c206bdeac093e
Nour-2003 <noureldeanh@gmail.com>
2024-11-26 22:03:33 +0200

    Navbar added

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { HomePageComponent } from "./home-page/home-page.component";
+import { IngredientsComponent } from "./ingredients/ingredients.component";
+import { RecipesComponent } from "./recipes/recipes.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: HomePageComponent },
+  { path: "ingredients", component: IngredientsComponent },
+  { path: "recipes", component: RecipesComponent },
+  // { path: "**", component: PageNotFoundComponent },
+];
