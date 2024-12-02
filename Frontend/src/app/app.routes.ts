5bc24ae7da46ff501cd63cfe301587b7e5586f6f
mo-ibrahim22 <mibrahimm002@gmail.com>
2024-12-02 03:00:16 +0200

    mainpage

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { IngredientsComponent } from './pages/ingredients/ingredients.component';
 import { RecipesComponent } from './pages/recipes/recipes.component';
 import { LoginComponent } from './pages/login/login.component';
+import { MainpageComponent } from './pages/mainpage/mainpage.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -10,5 +11,6 @@ export const routes: Routes = [
   { path: 'ingredients', component: IngredientsComponent },
   { path: 'recipes', component: RecipesComponent },
   { path: 'login', component: LoginComponent },
+  {path: 'mainpage', component:MainpageComponent}
   // { path: "**", component: PageNotFoundComponent },
 ];
