e70fefd07eacb2e55e499a0d7273d67b37ac536f
mo-ibrahim22 <mibrahimm002@gmail.com>
2024-12-02 09:01:33 +0200

    mainpage style

--- Frontend/src/app/pages/mainpage/mainpage.component.ts
+++ Frontend/src/app/pages/mainpage/mainpage.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-mainpage',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.css'
 })
