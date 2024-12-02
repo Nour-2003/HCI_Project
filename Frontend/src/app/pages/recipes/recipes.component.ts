b640c881ed09455582a83af787297042c9d362b2
mo-ibrahim22 <mibrahimm002@gmail.com>
2024-12-02 07:10:44 +0200

    recipe page

--- Frontend/src/app/pages/recipes/recipes.component.ts
+++ Frontend/src/app/pages/recipes/recipes.component.ts
@@ -1,12 +1,48 @@
-import { NgFor } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RecipeGridComponent } from '../../components/recipe-grid/recipe-grid.component';
+import { RecipeCardComponent } from "../../components/recipe-card/recipe-card.component";
 
 @Component({
   selector: 'app-recipes',
   standalone: true,
-  imports: [RecipeGridComponent],
+  imports: [RecipeGridComponent, RecipeCardComponent,CommonModule,NgFor],
   templateUrl: './recipes.component.html',
   styleUrl: './recipes.component.css',
 })
-export class RecipesComponent {}
+export class RecipesComponent {
+  recipes = [
+    {
+      title: 'Blueberry Pancakes',
+      imageUrl:
+        'https://i.dailymail.co.uk/1s/2019/11/05/19/20638190-7652901-image-m-35_1572981254783.jpg',
+      prepTime: '30 min',
+      difficulty: 'Easy',
+      likes: '1.7K',
+    },
+    {
+      title: 'Blueberry Pancakes',
+      imageUrl:
+        'https://i.dailymail.co.uk/1s/2019/11/05/19/20638190-7652901-image-m-35_1572981254783.jpg',
+      prepTime: '30 min',
+      difficulty: 'Easy',
+      likes: '1.7K',
+    },
+    {
+      title: 'Blueberry Pancakes',
+      imageUrl:
+        'https://i.dailymail.co.uk/1s/2019/11/05/19/20638190-7652901-image-m-35_1572981254783.jpg',
+      prepTime: '30 min',
+      difficulty: 'Easy',
+      likes: '1.7K',
+    },
+    {
+      title: 'Blueberry Pancakes',
+      imageUrl:
+        'https://i.dailymail.co.uk/1s/2019/11/05/19/20638190-7652901-image-m-35_1572981254783.jpg',
+      prepTime: '30 min',
+      difficulty: 'Easy',
+      likes: '1.7K',
+    },
+  ];
+}
