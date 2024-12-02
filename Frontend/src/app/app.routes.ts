0642e3d404cdbd9852063357dd0b0573a0b00104
mo-ibrahim22 <mibrahimm002@gmail.com>
2024-12-02 02:32:05 +0200

    loginpage

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,12 +1,14 @@
-import { Routes } from "@angular/router";
-import { HomePageComponent } from "./pages/home-page/home-page.component";
-import { IngredientsComponent } from "./pages/ingredients/ingredients.component";
-import { RecipesComponent } from "./pages/recipes/recipes.component";
+import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { IngredientsComponent } from './pages/ingredients/ingredients.component';
+import { RecipesComponent } from './pages/recipes/recipes.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
-  { path: "", redirectTo: "home", pathMatch: "full" },
-  { path: "home", component: HomePageComponent },
-  { path: "ingredients", component: IngredientsComponent },
-  { path: "recipes", component: RecipesComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'ingredients', component: IngredientsComponent },
+  { path: 'recipes', component: RecipesComponent },
+  { path: 'login', component: LoginComponent },
   // { path: "**", component: PageNotFoundComponent },
 ];
