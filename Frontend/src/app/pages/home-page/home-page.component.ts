64b8e4f57a8d41effb232ad8f88da668c5605053
mo-ibrahim22 <mibrahimm002@gmail.com>
2024-12-07 01:02:53 +0200

    home page ui

--- Frontend/src/app/pages/home-page/home-page.component.ts
+++ Frontend/src/app/pages/home-page/home-page.component.ts
@@ -1,47 +1,23 @@
 import { Component } from '@angular/core';
-import { MealsComponent } from "../../components/meals/meals.component";
-import { ChefListComponent } from "../../components/chef-list/chef-list.component";
-import { RecipeCardComponent } from "../../components/recipe-card/recipe-card.component";
+import { MealsComponent } from '../../components/meals/meals.component';
+import { ChefListComponent } from '../../components/chef-list/chef-list.component';
 import { CommonModule } from '@angular/common';
-import {HeaderComponent } from "../../components/header/header.component";
+import { HeaderComponent } from '../../components/header/header.component';
 import { HttpClientModule } from '@angular/common/http';
+import { RecipesComponent } from '../recipes/recipes.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [HeaderComponent, MealsComponent, ChefListComponent, RecipeCardComponent, CommonModule,HttpClientModule],
+  imports: [
+    HeaderComponent,
+    MealsComponent,
+    ChefListComponent,
+    CommonModule,
+    HttpClientModule,
+    RecipesComponent,
+  ],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrl: './home-page.component.css',
 })
-export class HomePageComponent {
-  trendingMeals = [
-    {
-      title: 'Blueberry Pancakes',
-      imageUrl: 'https://i.dailymail.co.uk/1s/2019/11/05/19/20638190-7652901-image-m-35_1572981254783.jpg',
-      prepTime: '30 min',
-      difficulty: 'Easy',
-      likes: '1.7K',
-    },
-    {
-      title: 'Blueberry Pancakes',
-      imageUrl: 'https://i.dailymail.co.uk/1s/2019/11/05/19/20638190-7652901-image-m-35_1572981254783.jpg',
-      prepTime: '30 min',
-      difficulty: 'Easy',
-      likes: '1.7K',
-    },
-    {
-      title: 'Blueberry Pancakes',
-      imageUrl: 'https://i.dailymail.co.uk/1s/2019/11/05/19/20638190-7652901-image-m-35_1572981254783.jpg',
-      prepTime: '30 min',
-      difficulty: 'Easy',
-      likes: '1.7K',
-    },
-    {
-      title: 'Blueberry Pancakes',
-      imageUrl: 'https://i.dailymail.co.uk/1s/2019/11/05/19/20638190-7652901-image-m-35_1572981254783.jpg',
-      prepTime: '30 min',
-      difficulty: 'Easy',
-      likes: '1.7K',
-    },
-  ];
-}
+export class HomePageComponent {}
